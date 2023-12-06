ac367beb3a01cc48ccc1ac20b3d053ff29eacc6a
Akhilesh <akhilesh13.1995@gmail.com>
2023-12-06 15:05:49 +0530

    Practice code

--- list/lists.py
+++ list/lists.py
@@ -17,20 +17,51 @@ this_list = list(("apple", "banana", "cherry"))
 """
 
 
-list1 = [10, 20, 30, 40]
-for list1[0] in list1:  # here list1[0] is just a iterator just like we use i, x or any other variable.
-    print(list1[0], end="\n")
+# list1 = [10, 20, 30, 40]
+# for list1[0] in list1:  # here list1[0] is just a iterator just like we use i, x or any other variable.
+#     print(list1[0], end="\n")
 
 
 # Swapping the list elements
-def swapList(str1):
+# def swapList(str1):
+#
+#     str1[0], str1[-1] = str1[-1], str1[0]
+#     return str1
+# str1 = [12, 35, 9, 56, 24]
+# print(swapList(str1))
+#
+#
+# #  The list() Constructor
+# newlist = list(("apple", "banana", "cherry")) # note the double round-brackets
+# print(newlist)
 
-    str1[0], str1[-1] = str1[-1], str1[0]
-    return str1
-str1 = [12, 35, 9, 56, 24]
-print(swapList(str1))
+
+# import sys
+#
+# x = True
+# size_of_x = sys.getsizeof(x)
+# print("size of x is : ", size_of_x)
 
 
-#  The list() Constructor
-newlist = list(("apple", "banana", "cherry")) # note the double round-brackets
-print(newlist)
+
+# 2 sum array Problem
+# https://leetcode.com/problems/two-sum/description/
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[j] == target - nums[i]:
+                    return [i, j]
+
+if __name__ == "__main__":
+    # Create an object of the Solution class
+    solution_obj = Solution()
+
+    # Example input: a list of numbers and a target
+    nums = [2, 7, 11, 15]
+    target = 9
+
+    # Call the twoSum method and print the result
+    result = solution_obj.twoSum(nums, target)
+    print("Indices of the two numbers:", result)
--- list/reverse_list.py
+++ list/reverse_list.py
@@ -1,16 +1,98 @@
 # Using libraries/direct method
 
-l1 = [10, 20, 30, 40, 50]
-l1.reverse()        # it will reverse the same list not create a new list
-print("Using reverse() : ", l1)
+# l1 = [10, 20, 30, 40, 50]
+# l1.reverse()        # it will reverse the same list not create a new list
+# print("Using reverse() : ", l1)
+#
+#
+# l2 = [10, 20, 30, 40, 50]
+# newl = list(l2.__reversed__())  # without converting into list <list_reverseiterator object at 0x000001FAAB9D7940>
+# print("Using reversed() : ", newl)  # will return a new list
+#
+# # using list slicing
+# l3 = [10, 20, 30, 40, 50]
+# print("Using list slicing : ", l3[::-1])
 
 
-l2 = [10, 20, 30, 40, 50]
-newl = list(l2.__reversed__())  # without converting into list <list_reverseiterator object at 0x000001FAAB9D7940>
-print("Using reversed() : ", newl)  # will return a new list
+# l1 = [10, 20, 30, 40, 50]
+# newarr = []
+# for i in l1:
+#     newarr.append(i[n])
 
-# using list slicing
-l3 = [10, 20, 30, 40, 50]
-print("Using list slicing : ", l3[::-1])
+# def some_func(*args, **kwargs):
+#     print(args)
+#     print(kwargs)
+# some_func(10, 20, 'foo', foo='bar', bar='100')
+
+# ar = ['a', 'b', 'c']
+# a = zip(range(len(ar)), ar)
+# a = list(a)
+# b = list(enumerate(ar))
+# is_equal = (a == b)
+# print(is_equal)
 
 
+
+import threading
+import time
+
+# def thread_function():
+#     print('Hi!')
+#     time.sleep(2)
+#     print('Good bye!')
+#
+# if __name__ == "__main__":
+#     print('start')
+#     x = threading.Thread(target=thread_function)
+#     x.start()
+#     print("Stop")
+
+
+# def thread_function(a, b):
+#     sum_of_square = 0
+#     for i in range(a, b):
+#         sum_of_square += a * b
+#     print(sum_of_square)
+#
+# if __name__ == "__main__":
+#     x = threading.Thread(target=thread_function, args=(1, 10000))
+#     x.start()
+#     y = threading.Thread(target=thread_function, args=(10000, 20000))
+#     y.start()
+
+
+# returns 'foo' first then 'bar' --> true or false
+import asyncio
+
+# async def say_after(delay, what):
+#     await asyncio.sleep(delay)
+#     print(what)
+#
+# async def foo():
+#     await say_after(2, 'foo')
+# async def bar():
+#     await say_after(1, 'bar')
+#
+# loop = asyncio.get_event_loop()
+# loop.run_until_complete(asyncio.wait([foo(), bar()]))
+# loop.close()
+
+
+
+# a, b = 10, 0
+# try:
+#     c = a/b
+# except Exception as exc:
+#     c = 5
+#
+# print(c)
+
+
+
+# fruits1 = ['apple', 'banana', 'ship', 'orange']
+# fruits2 = ['apple', 'banana']
+# fruits3 = [fruit for fruit in fruits1 if fruit not in fruits2]
+# print(fruits3)
+
+
+# print(testlist * 2 if testlist = ['abc', 33])
